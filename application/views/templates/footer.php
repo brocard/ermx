bacf0e075fe1655fb33e6d8123512f9ca7d5a604
Carlos Olmos <carlos@metistd.com>
2012-04-25 17:47:02 -0500

    leyenda

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,6 +5,7 @@
 <div class="alert alert-success separaForma">Ayúdanos a mejorar: <a href="https://twitter.com/ermx1" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @ermx1</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
   <center>
+  	<p> Proyecto sin fines de lucro organizado por </p>
     <p>&copy; Metis Studio. 2012</p>
   </center>
   <!-- diseñado por Metis Studio --> 
