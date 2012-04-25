7982c355dee197a0da88bd130f182273652787ae
Carlos Olmos <carlos@metistd.com>
2012-04-25 18:15:20 -0500

    intrucción

---
+++ application/logs/log-2012-04-25.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-04-25 18:15:00 --> Config Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Hooks Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Utf8 Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> UTF-8 Support Enabled
+DEBUG - 2012-04-25 18:15:00 --> URI Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Router Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> No URI present. Default controller set.
+DEBUG - 2012-04-25 18:15:00 --> Output Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Security Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Input Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> XSS Filtering completed
+DEBUG - 2012-04-25 18:15:00 --> XSS Filtering completed
+DEBUG - 2012-04-25 18:15:00 --> CRSF cookie Set
+DEBUG - 2012-04-25 18:15:00 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-04-25 18:15:00 --> Language Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Loader Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Controller Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Model Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Model Class Initialized
+DEBUG - 2012-04-25 18:15:00 --> Database Driver Class Initialized
+ERROR - 2012-04-25 18:15:00 --> Severity: Warning  --> mysql_pconnect(): Lost connection to MySQL server at 'reading initial communication packet', system error: 61 /usr/local/zend/apache2/htdocs/ermex/ermx/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2012-04-25 18:15:00 --> Unable to connect to the database
+DEBUG - 2012-04-25 18:15:00 --> Language file loaded: language/english/db_lang.php
